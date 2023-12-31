970c2a7bf236c7e7783ce2110b56ba12e0293e7e
tadashim <ma.tadashi@gmail.com>
2024-01-01 00:06:13 +0900

    creat calc

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,47 @@
-use std::collections::HashMap;
+enum Operation {
+    Add(f64, f64),
+    Subtract(f64, f64),
+    Multiply(f64, f64),
+    Divide(f64, f64),
+}
+
+impl Operation {
+    fn calculate(&self) -> f64 {
+        match self {
+            Operation::Add(a, b) => a + b,
+            Operation::Subtract(a, b) => a - b,
+            Operation::Multiply(a, b) => a * b,
+            Operation::Divide(a, b) => a / b,
+        }
+    }
+}
 
 fn main() {
-    let mut scores = HashMap::new();
+    println!("Enter the first number: ");
+    let mut first_number = String::new();
+    std::io::stdin().read_line(&mut first_number).expect("Failed to read line");
+    let first_number: f64 = first_number.trim().parse().expect("Please type a number!");
 
-    scores.insert(String::from("Alice"), 10);
-    scores.insert(String::from("Bob"), 20);
+    println!("Enter the operation: ");
+    let mut operation = String::new();
+    std::io::stdin().read_line(&mut operation).expect("Failed to read line");
+    let operation: char = operation.trim().parse().expect("Please type a number!");
+    
+    println!("Enter the second number: ");
+    let mut second_number = String::new();
+    std::io::stdin().read_line(&mut second_number).expect("Failed to read line");
+    let second_number: f64 = second_number.trim().parse().expect("Please type a number!");
 
-    for (key, value) in &scores {
-        println!("{}: {}", key, value);
-    }
+    // Create an instance of the Operation enum based on the user input.
+    // Call the calculate method on the Operation instance.
+    // Print the result to the terminal.
+    let op = match operation {
+        '+' => Operation::Add(first_number, second_number),
+        '-' => Operation::Subtract(first_number, second_number),
+        '*' => Operation::Multiply(first_number, second_number),
+        '/' => Operation::Divide(first_number, second_number),
+        _ => panic!("Invalid operation"),
+    };
+    let result = op.calculate();
+    println!("The result is {}", result);
 }
