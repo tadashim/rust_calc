6941032150cfd6717fb176c1265424bbe2603766
tadashim <ma.tadashi@gmail.com>
2023-12-31 18:53:01 +0900

    Result

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
-fn find_square_root(number: f64) -> Option<f64> {
-    if number >= 0.0 {
-        Some(number.sqrt())
+fn divide(a: f64, b: f64) -> Result<f64, String> {
+    if b == 0.0 {
+        Err("Cannot divide by zero!".to_string())
     } else {
-        None
+        Ok(a / b)
     }
 }
 
 fn main() {
-    let number = 4.0;
-    let square_root = find_square_root(number);
+    let a = 10.0;
+    let b = 2.0;
+    let division_result = divide(a, b);
 
-    match square_root {
-        Some(value) => println!("Square root of {} is {}", number, value),
-        None => println!("Cannot find square root of {}", number),
+    match division_result {
+        Ok(value) => println!("{} divide by {} is: {}", a, b, value),
+        Err(error_message) => println!("Error: {}", error_message),
     }
 }
